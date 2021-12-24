1a27ba7e6651f72edf5cc05d3ffebfc1642a9415
DeleMike <akindelemichael65@gmail.com>
2021-12-24 19:30:35 +0100

    build index page default look

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class IndexPage extends StatefulWidget {
+  static const routeName = '/index';
+  const IndexPage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _IndexPageState createState() => _IndexPageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _IndexPageState extends State<IndexPage> {
+  //selected tab
+  int _selectedTabIndex = 0;
+
+  //list of tabs
+  final List<Widget> _tabs = [
+    const Center(
+      child: Text('Home Page'),
+    ),
+    const Center(
+      child: Text('About Page'),
+    ),
+    const Center(
+      child: Text('Projects Page'),
+    ),
+  ];
+
+  void _onTabSelected(int index) {
+    setState(() {
+      _selectedTabIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      body: _tabs.elementAt(_selectedTabIndex),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            label: 'About',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.mobile_screen_share),
+            label: 'Share',
+          ),
+        ],
+        currentIndex: _selectedTabIndex,
+        selectedItemColor: Colors.blue[700],
+        onTap: _onTabSelected,
+      ),
     );
   }
 }
