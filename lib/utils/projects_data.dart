3895a08115cf18a45dd377614e13831eb8d8c8cd
DeleMike <akindelemichael65@gmail.com>
2021-12-26 21:35:57 +0100

    create project data

---
+++ lib/utils/projects_data.dart
@@ -0,0 +1,57 @@
+///Contains projects inoformation.
+final List<Map<String, Object>> projectsData = [
+  {
+    'projectID': 1,
+    'projectName': 'Trivia Game',
+    'projectDesc': 'A trivia platform to test your knowledge and have fun.'
+        'This project extensively uses The Provider for its state management.'
+        '\n\nBuilt and Powered with Flutter.',
+    'projectGtLink': 'https://github.com/DeleMike/trivia',
+    'projectImgCover': 'images/projects/trivia_jpg.jpg',
+  },
+  {
+    'projectID': 2,
+    'projectName': 'Coronavirus Report',
+    'projectDesc': 'Your one stop app for all breaking news on COVID-19.'
+        '\n\nBuilt and powered by Android Native SDK(Java)',
+    'projectGtLink': 'https://github.com/DeleMike/Coronavirus-Report',
+    'projectImgCover': 'images/projects/covid_jpg.jpg',
+  },
+  {
+    'projectID': 3,
+    'projectName': 'Video Player',
+    'projectDesc':
+        'This project builds a video player to showcase some of Flutter SDK Native powers.'
+            '\n\nBuilt and powered by Flutter',
+    'projectGtLink': 'https://github.com/DeleMike/vid_player',
+    'projectImgCover': 'images/projects/video_player.jpg',
+  },
+  {
+    'projectID': 4,
+    'projectName': 'Group Chat Messaging Application',
+    'projectDesc':
+        'A platform for group messaging. This project Showcase the use of '
+            'Firebase Authentication, Firestore for Data Persistence and Firebase Cloud Functions capabilities.'
+            '\n\nBuilt and powered by Flutter',
+    'projectGtLink': 'https://github.com/DeleMike/chat-app',
+    'projectImgCover': 'images/projects/chat.jpg',
+  },
+  {
+    'projectID': 5,
+    'projectName': 'Data Structures: Implementing the Stack and LinkedList',
+    'projectDesc': 'This project shows how to implement a Stack Data Structure.'
+        '\nIt also shows how to implement a LinkedList and uses a simple sorting algorithm(Bubble Sort) to sort data'
+        '\n\nBuilt and powered with C++',
+    'projectGtLink': 'https://github.com/DeleMike/Stack-DS-Implemtation',
+    'projectImgCover': 'images/projects/ds_algo.jpg',
+  },
+  {
+    'projectID': 6,
+    'projectName': 'Online Shop',
+    'projectDesc': 'A project to showcase shopping experience on the web. '
+        'Uses Firebase as its Backend.'
+        '\n\nBuilt and Powered with Flutter',
+    'projectGtLink': 'https://github.com/DeleMike/TheShop',
+    'projectImgCover': 'images/projects/online_shop.jpg',
+  },
+];
