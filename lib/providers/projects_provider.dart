3ebc752ac1f1ffbc9a4b99b5c7f3e4f1500c5e89
DeleMike <akindelemichael65@gmail.com>
2021-12-26 18:41:20 +0100

    create project provider

---
+++ lib/providers/projects_provider.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/foundation.dart';
+
+import '../screens/projects/projects_page.dart';
+import '../models/project.dart';
+
+/// Manages all functionalities of the [ProjectsPage]
+class ProjectProvider with ChangeNotifier {
+  // contains all project model
+  final List<Project> _projects = [];
+
+  ///returns a list of all Project model
+  List<Project> get projects {
+    return _projects;
+  }
+
+  /// Add a new Project
+  void addProject(Map<String, Object> projectsJson) {
+    final _project = Project.fromJson(projectsJson);
+    _projects.add(_project);
+    notifyListeners();
+  }
+
+  /// Fetch all projects
+  void fetchAndSetProjects() {
+    //get data
+    notifyListeners();
+  }
+}
